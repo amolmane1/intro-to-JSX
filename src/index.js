231b5eeeccfa854423e8724565de7fc3a8086f85
Amol Mane <amol1994mane@gmail.com>
2023-02-13 15:05:46 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello world</h1>
+    <p>Here we go</p>
+  </div>,
+  document.getElementById("root")
+);
+// ReactDOM.render(<h1>hello world</h1>, document.querySelector("#root"));
